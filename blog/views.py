82c4d1a6c000e1919ee80a44feb6d0b97e63ecbc
7stud <7stud@excite.com>
2015-12-29 15:07:09 -0700

    Added for loop to list_of_posts template.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 def list_of_posts(request):
-    return render(request, 'blog/list_of_posts.html', {})
+    posts = (Post.objects
+                 .filter(published_date__lte=timezone.now())
+                 .order_by('published_date')
+    )
+    return render(request, 'blog/list_of_posts.html', {'posts': posts})
